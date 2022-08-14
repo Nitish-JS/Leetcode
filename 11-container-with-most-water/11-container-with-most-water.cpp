23e5642c5372de785f3a1976f7ff6a916a9cb18f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-08-14 21:44:17 +0530

    Time: 97 ms (94.06%), Space: 59.1 MB (38.05%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,16 +4,14 @@ public:
         int i=0,j=arr.size()-1;
         int max_area=min(arr[i],arr[j])*(j-i);
         while(i<j){
-            int current_area;
             if(arr[i]<arr[j]){
                     i++;
-                current_area=min(arr[i],arr[j])*(j-i);
+                
             }
             else{
                     j--;
-               current_area=min(arr[i],arr[j])*(j-i);
             }
-            max_area=max(current_area,max_area);
+            max_area=max(min(arr[i],arr[j])*(j-i),max_area);
         }
         return max_area;
     }
