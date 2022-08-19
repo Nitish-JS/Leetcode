05dc5fdded1cbcd00a55fffd50a9a9a11c516abc
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-08-20 00:06:10 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (85.21%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int next(int n){
+        int ans=0;
+        while(n){
+            int rem=n%10;
+            ans+=rem*rem;
+            n/=10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        int slow;
+        int fast;
+        slow=fast=n;
+        do{
+            slow=next(slow);
+            fast=next(next(fast));
+        }while(slow!=fast);
+        if(slow==1)
+            return true;
+        else
+            return false;
+    }
+};
