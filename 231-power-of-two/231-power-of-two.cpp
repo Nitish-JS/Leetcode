3c3b52b93a3fe2c307c591f694a924f2e0198c8d
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-08-26 19:39:24 +0530

    Time: 9 ms (5.51%), Space: 6 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       return n>0 && (int)pow(2,30)%n==0;
+       // return n>0 && (int)pow(2,30)%n==0;
+        return n>0 && 1073741824%n==0;
     }
 };
