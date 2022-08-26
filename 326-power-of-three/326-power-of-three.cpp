1add183715f983fe7810f7d3483d1b4395089f58
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-08-26 20:15:01 +0530

    Time: 26 ms (75.03%), Space: 6.1 MB (12.98%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)
+        if(n<=0)
             return false;
-        while(n%3==0){
-            n=n/3;
-        }
-        return n==1;
+        return fmod(log10(n)/log10(3),1)==0;
     }
 };
