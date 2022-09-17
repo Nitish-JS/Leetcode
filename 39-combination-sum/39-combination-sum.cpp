89f24329edd86e7413366b847a1a660a0c75c9dd
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-09-17 14:13:17 +0530

    Time: 14 ms (56.86%), Space: 11 MB (88.25%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void combination(int index,int target,vector<int> &arr,vector<int> &ds){
+        if(index==arr.size()){
+            if(target==0)
+                ans.push_back(ds);
+            return;
+        }
+        if(arr[index]<=target){
+            ds.push_back(arr[index]);
+            combination(index,target-arr[index],arr,ds);
+            ds.pop_back();
+        }
+        combination(index+1,target,arr,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<int> ds;
+        combination(0,target,arr,ds);
+        return ans;
+    }
+};
