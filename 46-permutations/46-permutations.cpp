5d4e4f79d5a2feb9ee47231c8bdde6913064f546
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-09-22 01:25:55 +0530

    Time: 3 ms (83.35%), Space: 7.7 MB (78.43%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     void recurPermute(vector<int> &permArray,vector<int> &nums, vector<vector<int>> &ans,int freq[]){
-        if(permArray.size()==nums.size()){
-            ans.push_back(permArray);
-            return;
-        }
+       if(permArray.size()==nums.size()){
+           ans.push_back(permArray);
+           return;
+       }
         for(int i=0;i<nums.size();i++){
             if(!freq[i]){
                 permArray.push_back(nums[i]);
                 freq[i]=1;
                 recurPermute(permArray,nums,ans,freq);
-                freq[i]=0;
                 permArray.pop_back();
+                freq[i]=0;
+                // recurPermute(permArray,nums,ans,freq);
             }
+
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
