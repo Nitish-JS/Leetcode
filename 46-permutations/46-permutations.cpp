f9a06fabf54fe27e7dff0f768fa31aa9de2ffac7
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-09-22 14:35:24 +0530

    Time: 5 ms (52.89%), Space: 7.7 MB (78.43%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void recurPermute(vector<int> &permArray,vector<int> &nums, vector<vector<int>> &ans,int freq[]){
-       if(permArray.size()==nums.size()){
-           ans.push_back(permArray);
-           return;
-       }
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                permArray.push_back(nums[i]);
-                freq[i]=1;
-                recurPermute(permArray,nums,ans,freq);
-                permArray.pop_back();
-                freq[i]=0;
-                // recurPermute(permArray,nums,ans,freq);
-            }
-
+    void swap(int &a,int &b){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    void recurPermute(int index,vector<int> &nums,vector<vector<int>> &ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            recurPermute(index+1,nums,ans);
+            swap(nums[index],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> permArray;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++){
-            freq[i]=0;
-        }
-        recurPermute(permArray,nums,ans,freq);
+        // vector<int> permArray;
+        // int freq[nums.size()];
+        // for(int i=0;i<nums.size();i++){
+        //     freq[i]=0;
+        // }
+        recurPermute(0,nums,ans);
         return ans;
     }
 };
