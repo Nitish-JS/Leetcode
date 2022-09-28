d2534410474d7ace67ba0063bc859fb135c3b711
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-09-28 17:07:03 +0530

    Time: 382 ms (8.65%), Space: 66.9 MB (12.44%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int n=arr.size();
+       int n=arr.size();
         int left=0,right=n-1;
         int ans=0;
-        while(left<right && arr[left]<=arr[left+1] ){
+        while(left<n-1 && arr[left]<=arr[left+1]  ){
             left++;
         }
         if(left==n-1)
             return 0;
-        while(arr[right]>=arr[right-1] &&  right>0){
+        while(right>left && arr[right]>=arr[right-1]  ){
             right--;
         }
         ans=min(n-left-1,right);
         int i=0,j=right;
-       for(int i=0;i<=left;i++){
-           if(arr[i]<=arr[right])
-               ans=min(ans,right-i-1);
-           else if(right<n-1)
-               right++;
-           else
-               break;
-       }
+        while(i<=left && j<n){
+            if(arr[i]<=arr[j]){
+                ans=min(ans,j-i-1);
+                i++;
+            }
+            else
+                j++;
+        }
         return ans;
     }
 };
