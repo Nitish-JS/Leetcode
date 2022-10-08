b23a8ea568c17c19ab1b33e3de98da78e9559d7b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-10-08 10:43:03 +0530

    Time: 526 ms (5.49%), Space: 96.3 MB (97.73%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,30 +1,13 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if(nums.size()<=1)
-            return true;
-        int increasing=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]<=nums[i+1])
-                increasing=1;
-            else {
-                increasing=0;
-                break;                
-            }
+        bool increase = true;
+        bool decrease = true;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i] > nums[i+1]) increase = false;
+            if(nums[i] < nums[i+1]) decrease = false;
+            if(increase == false && decrease == false) return false;
         }
-        if(increasing)
-            return true;
-        int decreasing=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]>=nums[i+1])
-                decreasing=1;
-            else{
-                decreasing=0;
-                break;                
-            }
-        }
-        if(decreasing)
-            return true;
-        return false;
+        return true;
     }
 };
