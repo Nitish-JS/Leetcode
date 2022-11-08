c22bd8f60291513467165b0a5eabdb188ade55b4
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-11-08 15:58:25 +0530

    Time: 4 ms (64.51%), Space: 6.8 MB (24.11%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -4,10 +4,12 @@ public:
         if(s.size()==0)
             return s;
         string ans="";
-        for(int i=0;i<s.size();i++){
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
             if(abs((int)s[i]-(int)(s[i+1]))==32){
                 s=s.substr(0,i)+s.substr(i+2);
                 i=-1;
+                n-=2;
             }
         }
         return s;
