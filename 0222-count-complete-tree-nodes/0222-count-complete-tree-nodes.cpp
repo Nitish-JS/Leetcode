ef51b97ac7e83cbd7bec73dd2ee865db9027fe6f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-11-15 22:06:14 +0530

    Time: 74 ms (25.85%), Space: 30.9 MB (63.27%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,12 +11,16 @@
  */
 class Solution {
 public:
-    int answer(TreeNode *root){
+
+    int countNodes(TreeNode* root) {
         if(!root)
             return 0;
-        return answer(root->right)+answer(root->left)+1;
-    }
-    int countNodes(TreeNode* root) {
-        return answer(root);
+        int left_height=1,right_height=1;
+        TreeNode *left_tree=root,*right_tree=root;
+        while(left_tree=left_tree->left)left_height++;
+        while(right_tree=right_tree->right)right_height++;
+        if(left_height==right_height) return pow(2,left_height)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    
     }
 };
