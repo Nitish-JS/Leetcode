728b1b7ee87363c4648d3e8e5d631c5eb19e4a65
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-02 16:52:40 +0530

    Time: 196 ms (61.38%), Space: 20.4 MB (82.93%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -3,25 +3,19 @@ public:
     bool closeStrings(string word1, string word2) {
         if(word1.size()!=word2.size())
             return false;
-        map<int,int> m1;
-        map<int,int> m2;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        vector<bool> visited1(26,0);
+        vector<bool> visited2(26,0);
         for(int i=0;i<word1.size();i++){
-            m1[word1[i]]++;
-            m2[word2[i]]++;
-        }
-        vector<int> freq1;
-        vector<int> freq2;
-        for(auto it:m1){
-            if(m2.find(it.first)==m2.end())
-                return false;
-            freq1.push_back(it.second);
-        }
-         for(auto it:m2){
-            freq2.push_back(it.second);
+            freq1[word1[i]-'a']++;
+            freq2[word2[i]-'a']++;
+            visited1[word1[i]-'a']=true;
+            visited2[word2[i]-'a']=true;
         }
         sort(freq1.begin(),freq1.end());
         sort(freq2.begin(),freq2.end());
-        if(freq1==freq2)
+        if( freq1==freq2 && visited1==visited2)
             return true;
         else
             return false;
