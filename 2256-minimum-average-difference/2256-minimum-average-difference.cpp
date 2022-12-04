1e7259a3dbd8620c0bce89539a65af6cbb1a4a6b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-04 09:46:34 +0530

    Time: 358 ms (17.66%), Space: 91 MB (19.79%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        int n=nums.size();
+        vector<long long> prefix(n);
+        vector<long long> suffix(n);
+        
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                prefix[i]=prefix[i-1]+nums[i];
+                suffix[n-i-1]=nums[n-i-1]+suffix[n-i];
+            }else{
+                prefix[0]=nums[i];
+                suffix[n-1]=nums[n-1];
+            }
+        }
+        // for(int i:prefix){
+        //     cout<<i<<" ";
+        // }
+        // cout<<"\n";
+        // for(int i:suffix){
+        //     cout<<i<<" ";
+        // }
+        int index=-1;
+        long long ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            if(i==n-1){
+                if(prefix[i]/(n)<ans)
+                    index=i;
+            }
+            else{
+                int diff=abs((prefix[i]/(i+1)-suffix[i+1]/(n-i-1)));
+                if(diff<ans){
+                    index=i;
+                    ans=diff;
+                }
+            }
+        }
+            
+        return index;
+    }
+};
