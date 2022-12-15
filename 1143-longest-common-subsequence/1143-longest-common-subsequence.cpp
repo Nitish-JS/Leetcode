612473eeed755d0f147d801306800085c600952f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-15 13:22:07 +0530

    Time: 82 ms (10.71%), Space: 262 MB (12.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int lcs(string &text1,string &text2,int index1,int index2)
-    {
+
+    int lcs(string &text1,string &text2,int index1,int index2,vector<vector<int>> &dp){
         if(index1<0|| index2<0)
             return 0;
         if(dp[index1][index2]!=-1)
             return dp[index1][index2];
-        
-        if(text1[index1]==text2[index2]){
-            return dp[index1][index2]=1+lcs(text1,text2,index1-1,index2-1);       
+        else if(text1[index1]==text2[index2]){
+            return dp[index1][index2]=1+lcs(text1,text2,index1-1,index2-1,dp);       
         }
         else{
-           return  dp[index1][index2]=max(lcs(text1,text2,index1-1,index2),lcs(text1,text2,index1,index2-1));
+           return  dp[index1][index2]=max(lcs(text1,text2,index1-1,index2,dp),lcs(text1,text2,index1,index2-1,dp));
         }
             // return 
     }
     int longestCommonSubsequence(string text1, string text2) {
-        memset(dp,-1,sizeof(dp));
-        return lcs(text1,text2,text1.size()-1,text2.size()-1);
+        vector<vector<int>> dp(1000,vector<int>(1000,-1));
+       return lcs(text1,text2,text1.size()-1,text2.size()-1,dp);
     }
 };
