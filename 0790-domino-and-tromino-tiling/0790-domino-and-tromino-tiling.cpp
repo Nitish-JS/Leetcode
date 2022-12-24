5333ed82c51af668c567eca05e0878a11d94b0aa
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-24 12:30:12 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.21%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int numTilings(int n) {
-        int mod=1e9;
-        mod+=7;
-        long long dp[1001];
-        memset(dp,0,sizeof dp);
+        if(n==0)
+            return 0;
+        long long dp[n+1];
+        if(n==1)
+            return 1;
+        else if(n==2)
+            return 2;
         dp[1]=1;
         dp[2]=2;
         dp[3]=5;
-        if(n<=3)
-            return dp[n];
+        int mod=1e9;
+        mod+=7;
         for(int i=4;i<=n;i++){
-            dp[i]=2*dp[i-1]+dp[i-3];
+            dp[i]=(2*dp[i-1]+dp[i-3]);
             dp[i]%=mod;
         }
         return dp[n];
