a503e9e0a11bf0bd7e553c00a3efe4a87a3f5699
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-25 17:48:56 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n+1];
-        memset(dp,-1,sizeof(dp));
-        dp[0]=1;
-        dp[1]=1;
+        if(n<=1)
+            return 1;
+        int step1=1,step2=1;
+        int ans;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            ans=step1+step2;
+            step1=step2;
+            step2=ans;
         }
-        return dp[n];
+        return ans;
     }
 };
