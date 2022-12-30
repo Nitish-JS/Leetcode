00672db5690b285eb5e54fb5b1afc21fa15e9942
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-30 10:27:25 +0530

    Time: 4 ms (99.70%), Space: 10.7 MB (67.21%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &graph,vector<vector<int>> &ans,vector<int> &visited,int node){
+        visited.push_back(node);
+        if(node==graph.size()-1){
+            ans.push_back(visited);
+            visited.pop_back();
+            return;
+        }
+        else{
+            for(auto edge:graph[node]){
+                dfs(graph,ans,visited,edge);
+            }
+        }
+        visited.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> visited;
+        vector<vector<int>> ans;
+        dfs(graph,ans,visited,0);
+        return ans;
+    }
+};
