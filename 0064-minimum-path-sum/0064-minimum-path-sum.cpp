542947c35786f8b20fa7cb60339dd08ddac12785
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2022-12-31 08:57:38 +0530

    Time: 11 ms (81.43%), Space: 10.1 MB (57.48%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&grid,int i,int j,vector<vector<int>> &dp){
-        if(i==0 && j==0)
-            return grid[0][0];
-        if(i<0 || j<0)
-            return 1000;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int left=grid[i][j]+solve(grid,i,j-1,dp);
-        int right=grid[i][j]+solve(grid,i-1,j,dp);
-        return dp[i][j]=min(left,right);
-    }
+    // int solve(vector<vector<int>>&grid,int i,int j,vector<vector<int>> &dp){
+    //     if(i==0 && j==0)
+    //         return grid[0][0];
+    //     if(i<0 || j<0)
+    //         return 1000;
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     int left=grid[i][j]+solve(grid,i,j-1,dp);
+    //     int right=grid[i][j]+solve(grid,i-1,j,dp);
+    //     return dp[i][j]=min(left,right);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return solve(grid,grid.size()-1,grid[0].size()-1,dp);
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=grid[i][j];
+                else{
+                    int left=grid[i][j],up=grid[i][j];
+                    if(j>0)
+                        left+=dp[i][j-1];
+                    else
+                        left+=10000;
+                    if(i>0)
+                        up+=dp[i-1][j];
+                    else
+                        up+=10000;
+                    dp[i][j]=min(left,up);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
