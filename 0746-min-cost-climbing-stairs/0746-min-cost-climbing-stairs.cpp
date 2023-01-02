25ab032e9d3b11df3bdb7ad2a5489a7db82023c7
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-01-02 11:31:53 +0530

    Time: 4 ms (88.45%), Space: 13.7 MB (55.52%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int solve(int index,vector<int> &cost,vector<int> &dp){
-        if(index>=cost.size())
-            return 0;
-        if(dp[index]!=-1)
-            return dp[index];
-        int onestep=solve(index+1,cost,dp);
-        int twostep=solve(index+2,cost,dp);
-        return dp[index]=cost[index]+min(onestep,twostep);
-    }
+    // int solve(int index,vector<int> &cost,vector<int> &dp){
+    //     if(index>=cost.size())
+    //         return 0;
+    //     if(dp[index]!=-1)
+    //         return dp[index];
+    //     int onestep=solve(index+1,cost,dp);
+    //     int twostep=solve(index+2,cost,dp);
+    //     return dp[index]=cost[index]+min(onestep,twostep);
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(),-1);
-        int startZero=solve(0,cost,dp);
-        fill(dp.begin(),dp.end(),-1);
-        int startOne=solve(1,cost,dp);
-        return min(startZero,startOne);
+        int n=cost.size();
+        int prev2=cost[0];
+        int prev1=cost[1];
+        int cur;
+        for(int i=2;i<n;i++){
+            cur=cost[i]+min(prev1,prev2);
+            prev2=prev1;
+            prev1=cur;
+        }
+        return min(prev1,prev2);
     }
 };
