f4f6f3d1f64beb00d704293f818aa637117366fd
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-01-03 14:13:16 +0530

    Time: 44 ms (87.58%), Space: 12.1 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,31 +3,16 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int row=strs.size();
         int col=strs[0].size();
-        vector<vector<char>> mat(row,vector<char>(col));
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                mat[i][j]=strs[i][j];
-            }
-        }
-        vector<vector<char>> transpose(col,vector<char>(row));
-        for(int i=0;i<col;i++){
-            for(int j=0;j<row;j++){
-                transpose[i][j]=mat[j][i];
-            }
-        }
         int ans=0;
-        for(auto it:transpose){
-            vector<char> temp=it;
-            sort(temp.begin(),temp.end());
-            if(temp!=it)
-                ans++;
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row-1;j++){
+                if(strs[j][i]>strs[j+1][i]){
+                    ans++;
+                    break;
+                    
+                }
+            }
         }
-        // for(auto it:strs){
-        //     string temp=it;
-        //     sort(temp.begin(),temp.end());
-        //     if(temp!=it)
-        //         ans++;
-        // }
         return ans;
     }
 };
