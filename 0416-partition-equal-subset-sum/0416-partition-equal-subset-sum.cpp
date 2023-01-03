4648ba44413041cb893d0f36a3efdb6dd53d2e48
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-01-03 17:01:24 +0530

    Time: 574 ms (30.55%), Space: 9.8 MB (86.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,41 +1,6 @@
 class Solution {
 public:
-    bool solve(int n, int k, vector<int> &arr) {
-    vector<bool>prev(200001,0);
-    vector<bool> curr(200001,0);
-    prev[0]=true;
-    prev[arr[0]]=true;
-    for(int index=1;index<n;index++){
-        for(int target=1;target<=k;target++){
-            bool take=false;
-            if(arr[index]<=target)
-                take=prev[target-arr[index]];
-            bool nottake=prev[target];
-            curr[target]=take||nottake;
-        }
-        prev=curr;
-    }
-    return prev[k];
-    // Write your code here.
-}
-//     bool solve(vector<int> &nums,int index,int sum,vector<int> &dp){
-//         if(sum==0)
-//             return true;
-//         if(index==0){
-//             return nums[0]==sum;
-//         }
-//         if(sum<0)
-//             return false;
-//         if(dp[sum]!=-1)
-//             return dp[sum];
-//         bool take=false;
-        
-//         if(nums[index]<=sum){
-//             take=solve(nums,index-1,sum-nums[index],dp);
-//         }
-//         bool nottake=solve(nums,index-1,sum,dp);
-//         return dp[sum]=take||nottake;
-//     }
+//     bool solve(int n, int k, vector<int> &arr) {
     bool canPartition(vector<int>& nums) {
         int sum=0;
         for(auto it:nums){
@@ -43,6 +8,22 @@ public:
         }
         if(sum%2!=0)
             return false;
-        return solve(nums.size(),sum/2,nums);
+        sum/=2;
+        vector<bool> prev(sum+1,false);
+        vector<bool> curr(sum+1,false);
+        prev[0]=true;
+        curr[0]=true;
+        for(int index=1;index<nums.size();index++){
+            for(int target=1;target<=sum;target++){
+                bool take=false;
+                if(nums[index]<=target){
+                    take=prev[target-nums[index]];
+                }
+                bool nottake=prev[target];
+                curr[target]=take||nottake;
+            }
+            prev=curr;
+        }
+        return prev[sum];
     }
 };
