9bafd35f5c866001d34f213315ba3359d82ea3ea
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-02-07 22:27:11 +0530

    Time: 8 ms (97.24%), Space: 13.4 MB (15.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> vp;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==0)
-                    vp.push_back({i,j});
-            }
-        }
-        // cout<<matrix.size();
-        // for(auto it:vp){
-        //     cout<<it.first<<" "<<it.second<<'\n';
-        // }
-        for(auto it:vp){
-            int row=it.first;
-            for(int i=0;i<matrix[0].size();i++){
-                matrix[row][i]=0;
-            }
-            int col=it.second;
+            vector<int> row;
+            vector<int> col;
             for(int i=0;i<matrix.size();i++){
-                matrix[i][col]=0;
+                for(int j=0;j<matrix[0].size();j++){
+                    if(matrix[i][j]==0){
+                        row.push_back(i);
+                        col.push_back(j);
+                    }
+                }
+            }
+            for(int i:row){
+                for(int j=0;j<matrix[0].size();j++){
+                    matrix[i][j]=0;
+                }
+            }
+            for(int i:col){
+                for(int j=0;j<matrix.size();j++){
+                    matrix[j][i]=0;
+                }
             }
         }
-    }
 };
