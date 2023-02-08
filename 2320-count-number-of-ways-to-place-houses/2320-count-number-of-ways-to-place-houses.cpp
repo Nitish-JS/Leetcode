033df5d2504f9935ac202d856bb7e3c748a3d4cc
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-02-08 19:33:41 +0530

    Time: 3 ms (93.42%), Space: 5.9 MB (82.99%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -11,13 +11,14 @@ public:
     }
     int countHousePlacements(int n) {
         int mod=1e9+7;
-        vector<long long> dp(n+1,0);
-        dp[0]=1;
-        dp[1]=2;
+        long prev2=1;
+        long prev=2;
         for(int i=2;i<=n;i++){
-            dp[i]=(dp[i-1]+dp[i-2])%mod;
+            long curr=(prev+prev2)%mod;
+            prev2=prev;
+            prev=curr;
         }
        
-        return (dp[n]*dp[n])%mod;
+        return (prev*prev)%mod;
     }
 };
