2e68a2b043868033518580be6b14a939e6efb2b6
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-02-15 10:46:06 +0530

    Time: 1890 ms (5.00%), Space: 292.2 MB (16.71%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int index,int prev,vector<int>&nums,vector<vector<int>> &dp){
+        if(index>=nums.size())
+            return 0;
+        if(dp[index][prev+1]!=-1)
+            return dp[index][prev+1];
+        int take=0;
+        if(prev==-1 || nums[prev]<nums[index])
+            take=1+solve(index+1,index,nums,dp);
+        int nottake=solve(index+1,prev,nums,dp);
+        return dp[index][prev+1]=max(take,nottake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+        return solve(0,-1,nums,dp);
+    }
+};
