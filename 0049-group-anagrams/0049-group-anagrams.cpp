3a3deee9394806b35febce7c83788978c51aa54e
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-02-19 22:58:12 +0530

    Time: 33 ms (86.95%), Space: 20.7 MB (60.03%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(int i=0;i<strs.size();i++){
+            string s=strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(s);
+        }
+        for(auto it:mp)
+            ans.push_back(it.second);
+        return ans;
+        
+    }
+};
