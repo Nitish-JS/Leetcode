b3d60c362b559f944dfb2e787d145da08e56b7a1
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-03-05 15:24:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (34.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solve(int index,int n,vector<int> &dp){
-        if(index>=n-1)
+    int solve(int index,vector<int> &dp){
+        if(index<=0)
             return 1;
         if(dp[index]!=-1)
             return dp[index];
-        int oneStep=solve(index+1,n,dp);
-        int twoStep=solve(index+2,n,dp);
+        int oneStep=solve(index-1,dp);
+        int twoStep=solve(index-2,dp);
         return dp[index]=oneStep+twoStep;
     }
     int climbStairs(int n) {
         vector<int> dp(n,-1);
-       return solve(0,n,dp);
+       return solve(n-1,dp);
     }
 };
