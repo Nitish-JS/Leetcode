8f6939229b31d78c8f55c5fdff526c58e39ab15b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-03-25 14:29:54 +0530

    Time: 31 ms (98.04%), Space: 13.3 MB (60.34%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int maxSum(vector<vector<int>>& mat) {
         int n=mat.size();
-        int ans=INT_MIN;
+        int m=mat[0].size();
+        int ans=0;
         int sum=0;
         for(int i=0;i<n-2;i++){
-            for(int j=0;j<mat[0].size()-2;j++){
-                    sum=mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2];
-                    ans=max(ans,sum);
+            for(int j=0;j<m-2;j++){
+                sum=mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2];
+                ans=max(ans,sum);
             
         }
     }
