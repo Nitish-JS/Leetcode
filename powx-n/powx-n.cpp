622b392209a8840a9bf399153f4d4009a17f4104
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-04-23 01:02:12 +0530

    Time: 0 ms, Memory: 5.9 MB - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-             long m = n > 0 ? n : -(long)n;
-        double ans = 1.0;
-        while (m != 0) {
-            if (m % 2 != 0) // check if m is odd number
-                ans *= x; // 2¹ * 2² * 2⁴, etc
-            x *= x;
-            m /= 2; // binary search
+        double ans=1;
+        int m=long(abs(n));
+        while(m>0){
+            if(m%2==1) ans*=x;
+            m/=2;
+            x*=x;
         }
-        return n >= 0 ? ans : 1 / ans;
-        // double ans=1.0;
-        // int m=long(abs(n));
-        // while(m!=0){
-        //     if(m%2==1) ans*=x;
-        //     m/=2;
-        //     x*=x;
-        // }
-        // return n>=0?ans:1/ans;
+        return n>=0?ans:1/ans;
     }
      // long m = n > 0 ? n : -(long)n;
      //    double ans = 1.0;
@@ -28,5 +19,13 @@ public:
      //        m /= 2; // binary search
      //    }
      //    return n >= 0 ? ans : 1 / ans;
-
+     // long m = n > 0 ? n : -(long)n;
+     //    double ans = 1.0;
+     //    while (m != 0) {
+     //        if (m % 2 != 0) // check if m is odd number
+     //            ans *= x; // 2¹ * 2² * 2⁴, etc
+     //        x *= x;
+     //        m /= 2; // binary search
+     //    }
+     //    return n >= 0 ? ans : 1 / ans;
 };
