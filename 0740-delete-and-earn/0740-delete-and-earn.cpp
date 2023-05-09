a93a99c3799ea158ae71443731b4e9bce82d5efd
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-09 11:03:04 +0530

    Time: 10 ms (85.84%), Space: 11.8 MB (86.32%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -7,7 +7,7 @@ public:
             return sum_array[0];
         if(dp[index]!=-1)
             return dp[index];
-        int take=sum_array[index]+ solve(index-2,sum_array,dp);
+        int take=sum_array[index]+solve(index-2,sum_array,dp);
         int nottake=0+solve(index-1,sum_array,dp);
         return dp[index]=max(take,nottake);
         
@@ -16,11 +16,20 @@ public:
         int answer=INT_MIN;
         int maxi=*max_element(nums.begin(),nums.end());
         vector<int> sum_array(maxi+1,0);
-        vector<int> dp(maxi+1,-1);
+        vector<int> dp(maxi+1,0);
         for(int i:nums){
             sum_array[i]+=i;
         }
-        return solve(maxi,sum_array,dp);
+        dp[0]=0;
+        dp[1]=sum_array[1];
+        for(int index=2;index<maxi+1;index++){
+            int take=sum_array[index]+dp[index-2];
+            int nottake=0+dp[index-1];
+            dp[index]=max(take,nottake);
+        }
+        return dp[maxi];
+        // return solve(maxi,sum_array,dp);
+        
         
     }
 };
