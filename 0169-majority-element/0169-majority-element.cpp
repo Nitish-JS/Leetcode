d787fcaa1d3fb3dfe2b41b019e7a17efe8948f1b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-10 11:11:11 +0530

    Time: 19 ms (71.72%), Space: 19.6 MB (76.13%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int candidate=-1,vote=0;
+        for(int i=0;i<nums.size();i++){
+            if(vote==0){
+                candidate=nums[i];
+                vote=1;
+            }
+            else if(nums[i]==candidate)
+                vote++;
+            else
+                vote--;
+        }
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]=candidate){
+                count++;
+            }
+        }
+        return count>=nums.size()/2?candidate:-1;
+    }
+};
