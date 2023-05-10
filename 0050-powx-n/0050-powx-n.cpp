b61cc9c9a73e1798c8022f4720efcfadfe835472
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-10 13:14:46 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.84%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long temp=(long)n;
+        if(n<0) temp=(long)-1*n;
+        while(temp>0){
+            if(temp%2==0){
+                x=x*x;
+                temp/=2;
+            }
+            else{
+                ans=ans*x;
+                temp=temp-1;
+            }
+        }
+        return n>0?(double)ans:(double)1.0/ans;
+    }
+};
+     
