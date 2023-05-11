a8aa2c3df209f089a6675a9b2d5ec5221711fc84
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-11 12:53:42 +0530

    Time: 143 ms (40.01%), Space: 13.3 MB (48.96%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        set<vector<int>> st;
+        int n=nums.size();
+        for(int i=0;i<n-3;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                for(int j=i+1;j<n-2;j++){
+                    // if(j==1 || (j>1 && nums[j]!=nums[j-1])){
+                        int low=j+1;
+                        int high=nums.size()-1;
+                        while(low<high){
+                            if((long)nums[i]+nums[j]+nums[low]+nums[high]==target){
+                                st.insert({nums[i],nums[j],nums[low],nums[high]});
+                                while(low<high && nums[low]==nums[low+1]) low++;
+                                while(high>low && nums[high]==nums[high-1]) high--;
+                                low++;
+                                high--;
+                            }
+                            else if((long)nums[i]+nums[j]+nums[low]+nums[high]<target)
+                                low++;
+                            else
+                                high--;
+                        }
+                    // }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+    }
+};
