88d343f3c7aa0d5dd09a7fa069f575fd155e6d7e
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-11 12:56:00 +0530

    Time: 137 ms (40.59%), Space: 13.1 MB (84.98%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,16 +3,17 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         set<vector<int>> st;
+        vector<vector<int>> ans;
         int n=nums.size();
         for(int i=0;i<n-3;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 for(int j=i+1;j<n-2;j++){
-                    // if(j==1 || (j>1 && nums[j]!=nums[j-1])){
+                    if(j==i+1 || (j>i+1 && nums[j]!=nums[j-1])){
                         int low=j+1;
                         int high=nums.size()-1;
                         while(low<high){
                             if((long)nums[i]+nums[j]+nums[low]+nums[high]==target){
-                                st.insert({nums[i],nums[j],nums[low],nums[high]});
+                                ans.push_back({nums[i],nums[j],nums[low],nums[high]});
                                 while(low<high && nums[low]==nums[low+1]) low++;
                                 while(high>low && nums[high]==nums[high-1]) high--;
                                 low++;
@@ -23,11 +24,11 @@ public:
                             else
                                 high--;
                         }
-                    // }
+                    }
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(),st.end());
+        // vector<vector<int>> ans(st.begin(),st.end());
         return ans;
     }
 };
