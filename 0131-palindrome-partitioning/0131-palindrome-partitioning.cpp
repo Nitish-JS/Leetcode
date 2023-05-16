4f0ebadca05a16fc91caf8a306851c429cdb5af9
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-16 12:05:16 +0530

    Time: 137 ms (55.28%), Space: 57.6 MB (59.09%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     void solve(vector<vector<string>> &ans,vector<string> &substrings,string s,int index){
-        if(index==s.size()){
-            ans.push_back(substrings);
-            return ;
-        }
+       if(index==s.size()){
+           ans.push_back(substrings);
+           return ;
+       }
         for(int i=index;i<s.size();i++){
             if(checkPal(s,index,i)){
                 substrings.push_back(s.substr(index,i-index+1));
                 solve(ans,substrings,s,i+1);
                 substrings.pop_back();
             }
-           
         }
     }
     bool checkPal(string s,int start,int end){
-        while(start<=end){
-            if(s[start++]!=s[end--])
-                return false;
-        }
+       while(start<=end){
+           if(s[start++]!=s[end--])
+               return false;
+       }
         return true;
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
+       vector<vector<string>> ans;
         vector<string> substrings;
         solve(ans,substrings,s,0);
         return ans;
