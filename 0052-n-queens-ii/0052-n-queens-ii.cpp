44833eddc068787e7853f8f9ff2c025d6430f239
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-16 13:55:24 +0530

    Time: 7 ms (46.92%), Space: 6.6 MB (32.37%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string> &board){
+        int n=board.size();
+        int rowtemp=row;
+        int coltemp=col;
+        for(int i=col;i>=0;i--){
+            if(board[row][i]=='Q') return false;
+        }
+        while(rowtemp>=0 && coltemp>=0){
+            if(board[rowtemp--][coltemp--]=='Q') return false;
+        }
+        while(row<n && col>=0){
+            if(board[row++][col--]=='Q') return false;
+        }
+        return true;
+    }
+    
+    void solve(vector<vector<string>> &ans,vector<string> &board,int col,int n){
+        if(col==n){
+            ans.push_back(board);
+            return ;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board)){
+                board[row][col]='Q';
+                solve(ans,board,col+1,n);
+                board[row][col]='.';
+            }
+        }
+        
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(ans,board,0,n);
+        return ans.size();
+    }
+};
