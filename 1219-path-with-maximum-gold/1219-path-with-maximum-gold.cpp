721ef952f02ca458f8885ff95975d5a756479244
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-16 16:17:39 +0530

    Time: 358 ms (44.13%), Space: 7.6 MB (47.25%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() ||  grid[i][j]==0)
+            return 0;
+        int val=grid[i][j];
+        grid[i][j]=0;
+        int u=val+solve(i-1,j,grid,dp);
+        int d=val+solve(i+1,j,grid,dp);
+        int l=val+solve(i,j-1,grid,dp);
+        int r=val+solve(i,j+1,grid,dp);
+        grid[i][j]=val;
+        int ans=max(l,max(u,max(r,d)));
+        return dp[i][j]=ans;
+
+       
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        int maxi=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]==0) continue;
+                int x=solve(i,j,grid,dp);
+                maxi=max(maxi,x);
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }
+        return maxi;
+
+    }
+};
