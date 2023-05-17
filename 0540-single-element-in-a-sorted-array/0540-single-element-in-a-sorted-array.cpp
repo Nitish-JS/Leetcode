f6c0cd09b91aff94f1229f44bfb3139b687c79f1
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-17 12:02:57 +0530

    Time: 36 ms (14.59%), Space: 22.2 MB (90.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
-        if(arr.size()==1)
-            return arr[0];
-        int low=0,high=arr.size()-1;
+       
+        int low=0,high=arr.size()-2;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(mid%2==1){
