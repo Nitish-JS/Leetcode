552376719c5205fe4e9766207c8c17ef5a873ca1
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-19 00:04:05 +0530

    Time: 138 ms (31.83%), Space: 45.3 MB (99.48%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    void heapify(vector<int> &nums,int index,int n){
+        int largest=index;
+        int left=2*index+1;
+        int right=2*index+2;
+        if(left<n && nums[left]>nums[largest])
+            largest=left;
+        if(right<n && nums[right]>nums[largest])
+            largest=right;
+        if(largest!=index){
+            swap(nums[largest],nums[index]);
+            heapify(nums,largest,n);
+        }
+    }
+    int deleteHeap(vector<int> &nums){
+        int val=nums[0];
+        nums[0]=nums.back();
+        nums.pop_back();
+        heapify(nums,0,nums.size());
+        return val;
+    }
     int findKthLargest(vector<int>& nums, int k) {
-
-        priority_queue<int> pq;
-        for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]);
-        }
-        int ans;
-        while(k--){
-            ans=pq.top();
-            pq.pop();
-        }
+        int n=nums.size();
+        int start=n/2-1;
+        for(int i=start;i>=0;i--)
+            heapify(nums,i,n);
+        // for(int i:nums)
+        //     cout<<i<<" ";
+        int ans=0;
+        for(int i=0;i<k;i++)
+            ans=deleteHeap(nums);
         return ans;
     }
 };
