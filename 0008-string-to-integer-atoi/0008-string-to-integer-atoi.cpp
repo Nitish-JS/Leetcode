01d5e28ae694aab7da544ec3ce2cc24a7b04e78a
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-23 18:59:56 +0530

    Time: 0 ms (100.00%), Space: 7 MB (93.54%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,7 +3,7 @@ public:
     int myAtoi(string s) {
         int mod=1e9+7;
         int i=0;
-        double ans=0;
+        long ans=0;
         while(s[i]==' ')
             i++;
         bool pos=s[i]=='+';
@@ -11,11 +11,17 @@ public:
         pos==true?i++:i;
         neg==true?i++:i;
         while(i<s.size() && s[i]-'0'>=0 && s[i]-'0'<=9){
+            
             ans=(ans*10)+s[i++]-'0';
+            if(ans>INT_MAX){
+                if(neg)
+                    return INT_MIN;
+                else 
+                    return INT_MAX;
+                
+            }
         }
         ans=neg?ans*-1:ans;
-        if(ans<=INT_MIN) return INT_MIN;
-        else if(ans>=INT_MAX) return INT_MAX;
         return (int)ans;
     }
 };
