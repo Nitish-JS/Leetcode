c02b563bd29280c594048bc116a2add7eb4c56f2
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-23 19:47:59 +0530

    Time: 157 ms (32.65%), Space: 6.6 MB (97.91%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -14,17 +14,21 @@ public:
     // }
     int longestPalindromeSubseq(string s) {
         string rev=s;
-        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0));
+        // vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,0));
+        vector<int> curr(s.size()+1,0);
+        vector<int> prev(s.size()+1,0);
         reverse(rev.begin(),rev.end());
         for(int i=1;i<=s.size();i++){
             for(int j=1;j<=s.size();j++){
                 if(s[i-1]==rev[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=max(prev[j],curr[j-1]);
+                
             }
+            prev=curr;
         }
-        return dp[s.size()][rev.size()];
+        return curr[rev.size()];
         
     }
 };
