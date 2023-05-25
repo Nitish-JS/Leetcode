55143aef71946ce854e5980ce78cb9947a23e288
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-25 18:21:53 +0530

    Time: 13 ms (68.87%), Space: 9.1 MB (41.45%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,18 +7,13 @@ public:
             return i+1;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int match=INT_MAX;
-        int notMatch=INT_MAX;
-
         if(word1[i]==word2[j])
-            match=solve(word1,word2,i-1,j-1,dp);
-        else{
-            int insert=1+solve(word1,word2,i,j-1,dp);
-            int del=1+solve(word1,word2,i-1,j,dp);
-            int replace=1+solve(word1,word2,i-1,j-1,dp);
-            notMatch=min(insert,min(del,replace));
-        }
-        return dp[i][j]=min(match,notMatch);
+            return dp[i][j]=solve(word1,word2,i-1,j-1,dp);
+        int insert=1+solve(word1,word2,i,j-1,dp);
+        int del=1+solve(word1,word2,i-1,j,dp);
+        int replace=1+solve(word1,word2,i-1,j-1,dp);
+        return dp[i][j]=min(insert,min(del,replace));
+
     }
     int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
