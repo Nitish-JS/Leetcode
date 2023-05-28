bd9c7bebf8ab706157d57b8928d4e2311ec2294b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-05-28 23:54:14 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int solve(int n,int dp[]){
-        if(n<0)
-            return 0;
-        if(n<=1)
-            return dp[n]=n;
-        if(dp[n]!=-1)
-            return dp[n];
-        else
-            return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
+    // int solve(int n,int dp[]){
+    //     if(n<0)
+    //         return 0;
+    //     if(n<=1)
+    //         return dp[n]=n;
+    //     if(dp[n]!=-1)
+    //         return dp[n];
+    //     else
+    //         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    // }
     int fib(int n) {
-        int dp[31];
-        memset(dp,-1,sizeof(dp));
-        return solve(n,dp);
+        // int dp[31];
+        if(n<=1)
+            return n;
+        int f2=0,f1=1;
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=f2+f1;
+            f1=f2;
+            f2=ans;
+        }
+        return ans;
+        // memset(dp,-1,sizeof(dp));
+        // return solve(n,dp);
     }
 };
