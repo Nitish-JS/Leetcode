bacf5f38c836151f015163701bf2958574cb8b63
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-01 18:42:32 +0530

    Time: 10 ms (12.03%), Space: 11.1 MB (26.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,15 +6,15 @@ public:
             int mid=low+(high-low)/2;
             if(nums[mid]==target)
                 return mid;
-            else if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
+            else if(nums[low]<=nums[mid] ){
+                if(nums[low]<=target && nums[mid]>=target){
                     high=mid-1;
                 }
                 else
                     low=mid+1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[high]){
+                if(nums[mid]<=target && nums[high]>=target){
                     low=mid+1;
                 }
                 else
