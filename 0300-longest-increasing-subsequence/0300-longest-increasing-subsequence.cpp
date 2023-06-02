466cfb0e5cdc86457df08fe60d9ebcc0473cfb1a
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-02 13:02:21 +0530

    Time: 330 ms (55.24%), Space: 10.3 MB (86.77%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,8 +12,23 @@ public:
         return dp[index][prev+1]=max(pick,notpick);
         
     }
-    int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-       return solve(0,-1,nums,dp);
+    int lengthOfLIS(vector<int>& arr) {
+        int n=arr.size();
+         int dp[n];
+        fill(dp,dp+n,1);
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        for(int i:dp){
+            maxi=max(maxi,i);
+        }
+        return maxi;
+       //  vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+       // return solve(0,-1,nums,dp);
     }
 };
