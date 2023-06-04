f8e9c844aa6c7de22fb072742ebcad47a1416404
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-04 16:56:19 +0530

    Time: 480 ms (75.35%), Space: 8.1 MB (49.33%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>> &board,string &word,int i,int j,int wordIndex){
-        int n=board.size();
-        int m=board[0].size();
-        if(wordIndex==word.size())
+    bool solve(vector<vector<char>> &board,string &word,int wordIdx,int i,int j){
+        if(wordIdx==word.size())
             return true;
-        if(i<0 || j<0|| i>=n || j>=m || board[i][j]=='*' || board[i][j]!=word[wordIndex]) 
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]=='*' || board[i][j]!=word[wordIdx])
             return false;
+        // if(dp[i][j]!=-1)
+        //     return dp[i][j];
         char temp=board[i][j];
         board[i][j]='*';
-        bool up=solve(board,word,i-1,j,wordIndex+1);
-        bool down=solve(board,word,i+1,j,wordIndex+1);
-        bool right=solve(board,word,i,j+1,wordIndex+1);
-        bool left=solve(board,word,i,j-1,wordIndex+1);
+        bool up=solve(board,word,wordIdx+1,i-1,j);
+        bool down=solve(board,word,wordIdx+1,i+1,j);
+        bool left=solve(board,word,wordIdx+1,i,j-1);
+        bool right=solve(board,word,wordIdx+1,i,j+1);
         board[i][j]=temp;
-        return up || down|| right||left;
+        return up || down || left || right;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<int>> dp(board.size(),vector<int>(board[0].size(),-1));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(board[i][j]==word[0] && solve(board,word,i,j,0) ){
+                if(board[i][j]==word[0] && solve(board,word,0,i,j))
                     return true;
-                }
-                
             }
         }
         return false;
     }
+    
 };
