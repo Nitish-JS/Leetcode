8361f23f7470ca269df2608b6b5455ec292ab7bc
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-04 17:47:50 +0530

    Time: 172 ms (35.51%), Space: 95.8 MB (83.95%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n);
+        int prev=0;
+        int curr=0;
         int sum=0;
         for(int i=0;i<n;i++){
-            dp[0]+=nums[i]*i;
+            prev+=nums[i]*i;
             sum+=nums[i];
         }
-        int ans=dp[0];
+        int ans=prev;
         for(int i=1;i<n;i++){
-            dp[i]=dp[i-1]+sum-n*nums[n-i];
-            ans=max(ans,dp[i]);
+            curr=prev+sum-n*nums[n-i];
+            ans=max(ans,curr);
+            prev=curr;
         }
         return ans;
     }
