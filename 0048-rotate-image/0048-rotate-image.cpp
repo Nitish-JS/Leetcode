5addd6587484b68dfc054d976036839ea7eee4c2
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-04 17:05:17 +0530

    Time: 3 ms (43.39%), Space: 7 MB (89.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    void transpose(vector<vector<int>> &matrix){
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i<j)
+                    swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+            
+    }
     void rotate(vector<vector<int>>& matrix) {
-       for(int i=0;i<matrix.size();i++){
-           for(int j=0;j<i;j++){
-               swap(matrix[i][j],matrix[j][i]);
-           }
-       }
-        for(int i=0;i<matrix.size();i++){
+        int n=matrix.size();
+        transpose(matrix);
+        for(int i=0;i<n;i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
