578c708592de082d7ab58cab3d09204a1e3f43f5
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-10 15:39:30 +0530

    Time: 75 ms (83.93%), Space: 33 MB (29.61%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &grid,vector<vector<int>> &visited,int row,int col){
+        int m=grid.size();
+        int n=grid[0].size();
+        int dirX[]={-1,0,1,0};
+        int dirY[]={0,-1,0,1};
+        visited[row][col]=1;
+        for(int i=0;i<4;i++){
+            int newRow=row+dirX[i];
+            int newCol=col+dirY[i];
+            if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && !visited[newRow][newCol] && grid[newRow][newCol]==1)
+                dfs(grid,visited,newRow,newCol);
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1)
+                dfs(grid,visited,i,0);
+            if(grid[i][n-1]==1)
+                dfs(grid,visited,i,n-1);
+        }
+        
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==1)
+                dfs(grid,visited,0,i);
+            if(grid[m-1][i]==1)
+                dfs(grid,visited,m-1,i);
+        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<visited[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                // cout<<visited[i][j]<<" ";
+                if(grid[i][j]==1 && visited[i][j]==0)
+                    ans++;
+            }
+            // cout<<"\n";
+        }
+        return ans;
+    }
+};
