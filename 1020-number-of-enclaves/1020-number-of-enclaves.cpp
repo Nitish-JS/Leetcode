c96ac9762d287bed31bf46068399988d383047b8
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-10 15:41:44 +0530

    Time: 83 ms (57.55%), Space: 32.9 MB (33.88%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -18,24 +18,18 @@ public:
         int n=grid[0].size();
         vector<vector<int>> visited(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
-            if(grid[i][0]==1)
+            if(grid[i][0]==1 && !visited[i][0])
                 dfs(grid,visited,i,0);
-            if(grid[i][n-1]==1)
+            if(grid[i][n-1]==1 && !visited[i][n-1])
                 dfs(grid,visited,i,n-1);
         }
         
         for(int i=0;i<n;i++){
-            if(grid[0][i]==1)
+            if(grid[0][i]==1 && !visited[0][i])
                 dfs(grid,visited,0,i);
-            if(grid[m-1][i]==1)
+            if(grid[m-1][i]==1 && !visited[m-1][i])
                 dfs(grid,visited,m-1,i);
         }
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<visited[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
