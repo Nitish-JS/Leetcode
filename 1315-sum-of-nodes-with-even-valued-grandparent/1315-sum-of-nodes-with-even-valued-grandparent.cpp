fc4ad4da45b70706d450c92ff4c19adfda5ec014
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-15 11:47:13 +0530

    Time: 61 ms (22.02%), Space: 41.2 MB (97.36%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,28 +11,55 @@
  */
 class Solution {
 public:
-    int sumEvenGrandparent(TreeNode* root) {
-        queue<pair<TreeNode*,pair<TreeNode*,TreeNode*>>> q;
-        q.push({root,{NULL,NULL}});
-        int ans=0;
-        while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* root=q.front().first;
-                TreeNode* parent=q.front().second.first;
-                TreeNode* grandParent=q.front().second.second;
-                q.pop();
-                if(grandParent && (grandParent->val%2)==0)
-                    ans+=root->val;
-                if(root->left){
-                    q.push({root->left,{root,parent}});
-                }
-                if(root->right){
-                    q.push({root->right,{root,parent}});
-                }
-                
+    void solve(TreeNode *root,int &sum){
+        if(!root)
+            return;
+        if(root->left  && root->val%2==0){
+            if(root->left->left){
+                sum+=root->left->left->val;
+            }   
+            if(root->left->right){
+                sum+=root->left->right->val;
             }
         }
-        return ans;
+        if(root->right && root->val%2==0){
+            if(root->right->left){
+                sum+=root->right->left->val;
+            }   
+            if(root->right->right){
+                sum+=root->right->right->val;
+            }
+        }
+        solve(root->left,sum);
+        solve(root->right,sum);
+        
+    }
+    int sumEvenGrandparent(TreeNode* root) {
+        int sum=0;
+        solve(root,sum);
+        return sum;
+//         BFS Approach
+//         queue<pair<TreeNode*,pair<TreeNode*,TreeNode*>>> q;
+//         q.push({root,{NULL,NULL}});
+//         int ans=0;
+//         while(!q.empty()){
+//             int size=q.size();
+//             for(int i=0;i<size;i++){
+//                 TreeNode* root=q.front().first;
+//                 TreeNode* parent=q.front().second.first;
+//                 TreeNode* grandParent=q.front().second.second;
+//                 q.pop();
+//                 if(grandParent && (grandParent->val%2)==0)
+//                     ans+=root->val;
+//                 if(root->left){
+//                     q.push({root->left,{root,parent}});
+//                 }
+//                 if(root->right){
+//                     q.push({root->right,{root,parent}});
+//                 }
+                
+//             }
+//         }
+//         return ans;
     }
 };
