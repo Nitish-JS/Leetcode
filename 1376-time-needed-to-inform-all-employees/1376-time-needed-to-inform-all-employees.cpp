e63b377231dea0b9d55933585b2361dd4812bb3b
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-15 15:28:14 +0530

    Time: 363 ms (34.68%), Space: 117.5 MB (60.83%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int maxTime=INT_MIN;
-    void dfs(vector<int> adj[],vector<int> &informTime,int curr,int time ){
-        maxTime=max(time,maxTime);
-        for(auto it:adj[curr]){
-            dfs(adj,informTime,it,time+informTime[curr]);
-        }
-    }
+    // int maxTime=INT_MIN;
+    // void dfs(vector<int> adj[],vector<int> &informTime,int curr,int time ){
+    //     maxTime=max(time,maxTime);
+    //     for(auto it:adj[curr]){
+    //         dfs(adj,informTime,it,time+informTime[curr]);
+    //     }
+    // }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<int> adj[n];
         for(int i=0;i<n;i++){
@@ -14,7 +14,20 @@ public:
                 adj[manager[i]].push_back(i);
             }
         }
-        dfs(adj,informTime,headID,0);
+        // dfs(adj,informTime,headID,0);
+        int maxTime=INT_MIN;
+        int time=0;
+        queue<pair<int,int>> q;
+        q.push({headID,0});
+        while(!q.empty()){
+            int currManager=q.front().first;
+            int time=q.front().second;
+            q.pop();
+            maxTime=max(time,maxTime);
+            for(auto it:adj[currManager]){
+                q.push({it,time+informTime[currManager]});
+            }
+        }
         return maxTime;
     }
 };
