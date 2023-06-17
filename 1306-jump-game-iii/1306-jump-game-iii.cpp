dd92da9c18fbe489c6c1257c3493c2f117e5f2b6
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-17 09:56:12 +0530

    Time: 116 ms (6.78%), Space: 61.9 MB (5.04%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(vector<int> graph[],vector<int> &visited,int node,vector<int> &arr){
+        visited[node]=1;
+        if(arr[node]==0)
+            return true;
+        for(auto it:graph[node]){
+            if(!visited[it]){
+                if(solve(graph,visited,it,arr)==true)
+                    return true;
+            }
+        }
+        return false;
+       
+    }
+    bool canReach(vector<int>& arr, int start) {
+        int n=arr.size();
+        vector<int> graph[n];
+        for(int i=0;i<n;i++){
+            if(i+arr[i]<n && arr[i]!=0)
+                graph[i].push_back(i+arr[i]);
+            if(i-arr[i]>=0 && arr[i]!=0)
+                graph[i].push_back(i-arr[i]);
+        }
+
+        vector<int> visited(n,0);
+        return solve(graph,visited,start,arr);
+        
+    }
+};
