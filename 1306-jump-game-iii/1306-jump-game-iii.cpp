0d15b6df476e2aaeb285ae98ab8a24fe73d09cce
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-17 10:05:38 +0530

    Time: 132 ms (5.22%), Space: 46.5 MB (10.09%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    bool solve(vector<int> graph[],vector<int> &visited,int node,vector<int> &arr){
-        visited[node]=1;
-        if(arr[node]==0)
-            return true;
-        for(auto it:graph[node]){
-            if(!visited[it]){
-                if(solve(graph,visited,it,arr)==true)
-                    return true;
-            }
-        }
-        return false;
+    
+//     DFS Approach
+//     bool solve(vector<int> graph[],vector<int> &visited,int node,vector<int> &arr){
+//         visited[node]=1;
+//         if(arr[node]==0)
+//             return true;
+//         for(auto it:graph[node]){
+//             if(!visited[it]){
+//                 if(solve(graph,visited,it,arr)==true)
+//                     return true;
+//             }
+//         }
+//         return false;
        
-    }
+//     }
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
         vector<int> graph[n];
@@ -24,7 +26,23 @@ public:
         }
 
         vector<int> visited(n,0);
-        return solve(graph,visited,start,arr);
+        queue<int> q;
+        q.push(start);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(arr[node]==0){
+                return true;
+            }
+            visited[node]=1;
+            for(auto it:graph[node]){
+                if(!visited[it]){
+                    q.push(it);
+                }
+            }
+        }
+        return false;
+        // return solve(graph,visited,start,arr);
         
     }
 };
