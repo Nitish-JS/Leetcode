ba6f5694df06b3e35c790bd33bb47132c3061704
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-17 12:09:59 +0530

    Time: 138 ms (29.21%), Space: 38.7 MB (41.39%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        unordered_set<int> adj[n];
+        vector<int> degree(n,0);
+        for(int i=0;i<roads.size();i++){
+            adj[roads[i][0]].insert(roads[i][1]);
+            adj[roads[i][1]].insert(roads[i][0]);
+            degree[roads[i][0]]++;
+            degree[roads[i][1]]++;
+        }
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    int rank=0;
+                    if(adj[i].find(j)!=adj[i].end())
+                        rank=degree[i]+degree[j]-1;
+                    else
+                        rank=degree[i]+degree[j];
+                    ans=max(rank,ans);
+                }
+            }
+        }
+        return ans;
+    }
+};
