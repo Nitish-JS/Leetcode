cef0d8fa1f58630d7896cc3847ff69a597467b0f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-20 19:36:39 +0530

    Time: 179 ms (18.88%), Space: 55.1 MB (20.57%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+    void buildGraph(vector<int> adj[],vector<vector<int>> &connections){
+        for(auto edge:connections){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+    }
+    void dfs(vector<int> adj[],vector<int> &visited,int node){
+        visited[node]=1;
+        for(auto it:adj[node]){
+            if(!visited[it]){
+                dfs(adj,visited,it);
+            }
+        }
+    }
+    void bfs(vector<int> adj[],vector<int> &visited,int node){
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int currNode=q.front();
+            q.pop();
+            visited[currNode]=1;
+            for(auto it:adj[currNode]){
+                if(!visited[it]){
+                    q.push(it);
+                }
+            }
+        }
+    }
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size()<n-1)
+            return -1;
+        vector<int> adj[n];
+        buildGraph(adj,connections);
+        int connectedComponents=0;
+        vector<int> visited(n);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                connectedComponents++;
+                dfs(adj,visited,i);
+            }
+        }
+        return connectedComponents-1;
+    }
+};
