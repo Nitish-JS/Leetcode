ab92b4916f212ce54b9679b0873e22578a9f178a
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-21 23:41:42 +0530

    Time: 150 ms (46.24%), Space: 43.9 MB (47.55%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,51 +1,98 @@
+class DisjointSet{
+    vector<int> parent,rank;
+public:
+    DisjointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int x){
+        if(parent[x]==x)
+            return x;
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int u,int v){
+        int ulti_parent_u=find(u);
+        int ulti_parent_v=find(v);
+        // both are in same component
+        if(ulti_parent_u==ulti_parent_v)
+            return;
+        if(rank[ulti_parent_u]<rank[ulti_parent_v]){
+            parent[ulti_parent_u]=ulti_parent_v;
+        }
+        else if(rank[ulti_parent_u]>rank[ulti_parent_v]){
+            parent[ulti_parent_v]=ulti_parent_u;
+        }
+        else{
+            parent[ulti_parent_v]=ulti_parent_u;
+            rank[ulti_parent_u]++;
+        }
+    }
+    
+};
+
 class Solution {
 private:
-    void buildGraph(vector<int> adj[],vector<vector<int>> &connections){
-        for(auto edge:connections){
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-        }
-    }
-//     DFS Approach
-    void dfs(vector<int> adj[],vector<int> &visited,int node){
-        visited[node]=1;
-        for(auto it:adj[node]){
-            if(!visited[it]){
-                dfs(adj,visited,it);
-            }
-        }
-    }
+//     void buildGraph(vector<int> adj[],vector<vector<int>> &connections){
+//         for(auto edge:connections){
+//             adj[edge[0]].push_back(edge[1]);
+//             adj[edge[1]].push_back(edge[0]);
+//         }
+//     }
+//      DFS Approach
+//     void dfs(vector<int> adj[],vector<int> &visited,int node){
+//         visited[node]=1;
+//         for(auto it:adj[node]){
+//             if(!visited[it]){
+//                 dfs(adj,visited,it);
+//             }
+//         }
+//     }
 //     BFS Approach gives TLE
-    void bfs(vector<int> adj[],vector<int> &visited,int node){
-        queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int currNode=q.front();
-            q.pop();
-            visited[currNode]=1;
-            for(auto it:adj[currNode]){
-                if(!visited[it]){
-                    q.push(it);
-                }
-            }
-        }
-    }
+//     void bfs(vector<int> adj[],vector<int> &visited,int node){
+//         queue<int> q;
+//         q.push(node);
+//         while(!q.empty()){
+//             int currNode=q.front();
+//             q.pop();
+//             visited[currNode]=1;
+//             for(auto it:adj[currNode]){
+//                 if(!visited[it]){
+//                     q.push(it);
+//                 }
+//             }
+//         }
+//     }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1)
-            return -1;
-        vector<int> adj[n];
-        buildGraph(adj,connections);
-        int connectedComponents=0;
-        vector<int> visited(n);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                connectedComponents++;
-                dfs(adj,visited,i);
-//              bfs(adj,visited,i); TLE 
-//              TRY UNION FIND
-            }
+             return -1;
+//         Disjoint set approach
+        DisjointSet DS(n);
+        for(auto it:connections){
+            DS.Union(it[0],it[1]);
         }
-        return connectedComponents-1;
+        unordered_set<int> unionSize;
+        for(int i=0;i<n;i++){
+            unionSize.insert(DS.find(i));
+        }
+        return unionSize.size()-1;
+//         DFS approach
+
+//         vector<int> adj[n];
+//         buildGraph(adj,connections);
+//         int connectedComponents=0;
+//         vector<int> visited(n);
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 connectedComponents++;
+//                 dfs(adj,visited,i);
+// //              bfs(adj,visited,i); TLE 
+// //              TRY UNION FIND
+//             }
+//         }
+//         return connectedComponents-1;
     }
 };
