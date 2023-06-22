c6225aab8bb641155911b2ff7740b1976f9005a7
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-22 10:47:46 +0530

    Time: 254 ms (40.86%), Space: 90.8 MB (58.01%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    int solve(vector<int> &prices,int fee,int index,int buy,vector<vector<int>> &dp){
-        if(index>=prices.size())
-            return 0;
-        if(dp[index][buy]!=-1)
-            return dp[index][buy];
-        int take,nottake;
-        if(buy){
-            take=-prices[index]+solve(prices,fee,index+1,0,dp);
-            nottake=0+solve(prices,fee,index+1,1,dp);
-        }
-        else{
-            take=prices[index]-fee+solve(prices,fee,index+1,1,dp);
-            nottake=0+solve(prices,fee,index+1,0,dp);
-        }
-        return dp[index][buy]=max(take,nottake);
-    }
+//     recursion + memoization
+    // int solve(vector<int> &prices,int fee,int index,int buy,vector<vector<int>> &dp){
+    //     if(index>=prices.size())
+    //         return 0;
+    //     if(dp[index][buy]!=-1)
+    //         return dp[index][buy];
+    //     int take,nottake;
+    //     if(buy){
+    //         take=-prices[index]+solve(prices,fee,index+1,0,dp);
+    //         nottake=0+solve(prices,fee,index+1,1,dp);
+    //     }
+    //     else{
+    //         take=prices[index]-fee+solve(prices,fee,index+1,1,dp);
+    //         nottake=0+solve(prices,fee,index+1,0,dp);
+    //     }
+    //     return dp[index][buy]=max(take,nottake);
+    // }
     int maxProfit(vector<int>& prices, int fee) {
+//         tabulation
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(prices,fee,0,1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        for(int index=n-1;index>=0;index--){
+            for(int buy=0;buy<2;buy++){
+                int take,nottake;
+                if(buy){
+                    take=-prices[index]+dp[index+1][0];
+                    nottake=0+dp[index+1][1];
+                }
+                else{
+                    take=prices[index]-fee+dp[index+1][1];
+                    nottake=0+dp[index+1][0];
+                }  
+                dp[index][buy]=max(take,nottake);
+            }
+        }
+        return dp[0][1];
+        // return solve(prices,fee,0,1,dp);
     }
 };
