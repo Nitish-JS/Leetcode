dc1b0a7dd9630e8f90a4640731de98b5aff168e7
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-23 00:34:34 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (89.68%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]!=' ')
+                length++;
+            else{
+                if(length>0)
+                    break;
+            }
+        }
+        return length;
+    }
+};
