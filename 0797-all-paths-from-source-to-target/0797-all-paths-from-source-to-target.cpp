f26033a567763fdcd47ea7bb053f64f358745ee0
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-25 00:24:11 +0530

    Time: 24 ms (37.20%), Space: 16.5 MB (21.31%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,23 +1,21 @@
 class Solution {
+vector<vector<int>> ans;
 public:
-    void dfs(vector<vector<int>> &graph,vector<vector<int>> &ans,vector<int> &visited,int node){
-        visited.push_back(node);
-        if(node==graph.size()-1){
-            ans.push_back(visited);
-            visited.pop_back();
+    void dfs(vector<vector<int>> &graph,vector<int> temp,int node,int n){
+        temp.push_back(node);
+        if(node==n){
+            ans.push_back(temp);
             return;
         }
-        else{
-            for(auto edge:graph[node]){
-                dfs(graph,ans,visited,edge);
-            }
-        }
-        visited.pop_back();
+        for(auto it:graph[node]){
+           dfs(graph,temp,it,n);
+        }      
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int> visited;
-        vector<vector<int>> ans;
-        dfs(graph,ans,visited,0);
+        int n=graph.size()-1;
+        vector<int> visited(n+1,0);
+        vector<int> temp;
+        dfs(graph,temp,0,n);
         return ans;
     }
 };
