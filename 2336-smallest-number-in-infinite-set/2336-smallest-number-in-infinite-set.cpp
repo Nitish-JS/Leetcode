e21f79d94f3fee1336eeea7acb4aa84997fbb330
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-06-27 11:22:16 +0530

    Time: 79 ms (83.16%), Space: 35.4 MB (75.94%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,22 +1,49 @@
+// optimised approach
 class SmallestInfiniteSet {
 public:
     set<int> st;
+    int cur;
     SmallestInfiniteSet() {
-        for(int i=1;i<=1000;i++){
-            st.insert(i);
-        }
+        cur=1;
     }
     
     int popSmallest() {
-        int ans=*(st.begin());
-        st.erase(ans);
-        return ans;
+        if(st.size()>0){
+            int ans=*(st.begin());
+            st.erase(ans);
+            return ans;
+        }
+        else{
+            cur+=1;
+            return cur-1;
+        }
     }
     
     void addBack(int num) {
-       st.insert(num);
+        if(cur>num)
+            st.insert(num);
     }
 };
+
+// class SmallestInfiniteSet {
+// public:
+//     set<int> st;
+//     SmallestInfiniteSet() {
+//         for(int i=1;i<=1000;i++){
+//             st.insert(i);
+//         }
+//     }
+    
+//     int popSmallest() {
+//         int ans=*(st.begin());
+//         st.erase(ans);
+//         return ans;
+//     }
+    
+//     void addBack(int num) {
+//        st.insert(num);
+//     }
+// };
 
 /**
  * Your SmallestInfiniteSet object will be instantiated and called as such:
