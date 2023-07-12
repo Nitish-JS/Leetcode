e505f61fb0ea3c66a7b9e944e1a45431388291f7
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-13 00:08:04 +0530

    Time: 155 ms (94.73%), Space: 47.7 MB (81.88%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool isCyclic(vector<vector<int>> &graph,vector<int> &visited,vector<int> &dfsVisited,int node){
+    bool isCycle(vector<vector<int>> &graph,vector<int> &visited,vector<int> &dfsVisited,int node){
         visited[node]=1;
         dfsVisited[node]=1;
         for(auto it:graph[node]){
             if(!visited[it]){
-                if(isCyclic(graph,visited,dfsVisited,it)==true)
+                if(isCycle(graph,visited,dfsVisited,it)==true)
                     return true;
             }
-           else if(dfsVisited[it])
-               return true;
+            else if(dfsVisited[it])
+                return true;
         }
         dfsVisited[node]=0;
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> safeNodes;
-        int V=graph.size();
-        vector<int> visited(V);
-        vector<int> dfsVisited(V);
-        for(int i=0;i<V;i++){
-            if(isCyclic(graph,visited,dfsVisited,i)==false)
-                safeNodes.push_back(i);
+        vector<int> ans;
+        int n=graph.size();
+        vector<int> visited(n);
+        vector<int> dfsVisited(n);
+        for(int i=0;i<n;i++){
+            if(isCycle(graph,visited,dfsVisited,i)==false)
+                ans.push_back(i);
         }
-        return safeNodes;
-        
-        
-        
+        return ans;
     }
 };
