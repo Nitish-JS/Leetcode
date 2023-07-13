86d7c28ffa2cc355ac5dd69c85d18285ca9a7d6f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-13 22:42:22 +0530

    Time: 25 ms (62.83%), Space: 14.6 MB (22.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,63 +1,32 @@
 class Solution {
 public:
-    // dfs approach
-    // bool detectCycle(int node,vector<int> adj[],vector<int> &visited,vector<int> &dfs){
-    //     visited[node]=1;
-    //     dfs[node]=1;
-    //     for(int it:adj[node]){
-    //         if(!visited[it]){
-    //             if(detectCycle(it,adj,visited,dfs))
-    //                 return true;
-    //         }
-    //         else if(dfs[it])
-    //             return true;
-    //     }
-    //     dfs[node]=0;
-    //     return false;
-    // }
-    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    //     int n=numCourses;
-    //     vector<int> adj[n];
-    //     for(auto edge:prerequisites){
-    //         adj[edge[1]].push_back(edge[0]);
-    //     }
-    //    vector<int> visited(n),dfs(n);
-    //     for(int i=0;i<n;i++){
-    //         if(!visited[i] && detectCycle(i,adj,visited,dfs))
-    //             return false;
-    //     }
-    //     return true;
-    // }
-    
-    // bfs approach
-    bool canFinish(int V, vector<vector<int>>& prerequisites) {
-        vector<int> adj[V];
-        for(auto edge:prerequisites){
-            adj[edge[1]].push_back(edge[0]);
-        }
-        vector<int> indegree(V);
-        for(int i=0;i<V;i++){
-            for(int it:adj[i]){
-                indegree[it]++;
+    bool isCycle(vector<vector<int>> &graph,vector<int> &visited,vector<int> &dfsVisited,int node){
+        visited[node]=1;
+        dfsVisited[node]=1;
+        for(auto it:graph[node]){
+            if(!visited[it]){
+                if(isCycle(graph,visited,dfsVisited,it))
+                    return true;
             }
+            else if(dfsVisited[it])
+                return true;
         }
-        queue<int> q;
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0)
-                q.push(i);
-        }
-        int count=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            count++;
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
-            }
-        }
-        return count==V?true:false;
+        dfsVisited[node]=0;
+        return false;
     }
-    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V=numCourses;
+        vector<vector<int>> graph(V);
+        for(auto edge:prerequisites){
+            graph[edge[1]].push_back(edge[0]);
+        }        
+        vector<int> visited(V);
+        vector<int> dfsVisited(V);
+        for(int i=0;i<V;i++){
+            if(!visited[i] && isCycle(graph,visited,dfsVisited,i))
+                return false;
+        }
+        return true;
+        
+    }
 };
