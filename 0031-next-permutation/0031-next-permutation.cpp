11b912cc64f86b79cc9f5e04fda660abb78055c2
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-18 00:20:31 +0530

    Time: 4 ms (71.96%), Space: 12.2 MB (19.85%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
+    void nextPermutation(vector<int>& nums) {
         int breakpoint=-1;
-        for(int i=arr.size()-1;i>0;i--){
-            if(arr[i]>arr[i-1]){
-                breakpoint=i-1;
-                break;                
-            }
-        }
-        if(breakpoint==-1){
-            reverse(arr.begin(),arr.end());
-            return;
-        }
-        for(int i=arr.size()-1;i>=0;i--){
-            if(arr[i]>arr[breakpoint]){
-                swap(arr[breakpoint],arr[i]);
-                reverse(arr.begin()+breakpoint+1,arr.end());
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                breakpoint=i;
                 break;
             }
         }
-        
-        
+        if(breakpoint==-1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]>nums[breakpoint]){
+                swap(nums[i],nums[breakpoint]);
+                break;
+            }
+        }
+        sort(nums.begin()+breakpoint+1,nums.end());
     }
 };
