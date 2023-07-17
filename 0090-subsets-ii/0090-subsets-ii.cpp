7877be4088cd0fca5d1bafb0e2d31a54bae92ca4
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-18 01:04:14 +0530

    Time: 3 ms (69.70%), Space: 7.3 MB (99.50%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    
-    void powerSet(vector<int> &nums,int index,set<vector<int>> &ans,vector<int> &subset){
-        if(index<0){
-            ans.insert(subset);
-            return ;
+    // void powerSet(vector<int> &nums,int index,set<vector<int>> &ans,vector<int> &subset){
+    //     if(index<0){
+    //         ans.insert(subset);
+    //         return ;
+    //     }
+    //     subset.push_back(nums[index]);
+    //     powerSet(nums,index-1,ans,subset);
+    //     subset.pop_back();
+    //     powerSet(nums,index-1,ans,subset);
+    // }
+        void powerSet(vector<int> &nums,int index,vector<vector<int>> &ans,vector<int> &subset){
+            ans.push_back(subset);
+            for(int i=index;i>=0;i--){
+                if(i<index && nums[i]==nums[i+1]) continue;
+                subset.push_back(nums[i]);
+                powerSet(nums,i-1,ans,subset);
+                subset.pop_back();
+            }
+            
         }
-        subset.push_back(nums[index]);
-        powerSet(nums,index-1,ans,subset);
-        subset.pop_back();
-        powerSet(nums,index-1,ans,subset);
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans;
         vector<int> subset;
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         powerSet(nums,nums.size()-1,ans,subset);
-        vector<vector<int>> subsets(ans.begin(),ans.end());
-        return subsets;
+        return ans;
+        // set<vector<int>> ans;
+        // vector<int> subset;
+        // sort(nums.begin(),nums.end());
+        // powerSet(nums,nums.size()-1,ans,subset);
+        // vector<vector<int>> subsets(ans.begin(),ans.end());
+        // return subsets;
     }
 };
