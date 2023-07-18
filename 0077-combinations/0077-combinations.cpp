54c04aa2b03e6363f7e1c7dc77a79d6fdcd44957
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-18 10:55:26 +0530

    Time: 29 ms (67.58%), Space: 17.7 MB (39.01%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &ans,vector<int> &nums,int index,int k,vector<int> &ds){
-        if(index==nums.size() || ds.size()==k){
+    void solve(vector<vector<int>> &ans,int n,int index,int k,vector<int> &ds){
+        if(index==n+1 || ds.size()==k){
             if(ds.size()==k){
                 ans.push_back(ds);
             }
             return ;
         }
-        ds.push_back(nums[index]);
-        solve(ans,nums,index+1,k,ds);
+        ds.push_back(index);
+        solve(ans,n,index+1,k,ds);
         ds.pop_back();
-        solve(ans,nums,index+1,k,ds);
+        solve(ans,n,index+1,k,ds);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums(n);
-        for(int i=0;i<n;i++){
-            nums[i]=i+1;
-        }
+        // vector<int> nums(n);
+        // for(int i=0;i<n;i++){
+        //     nums[i]=i+1;
+        // }
         vector<vector<int>> ans;
         vector<int> ds;
-        solve(ans,nums,0,k,ds);
+        solve(ans,n,1,k,ds);
         return ans;
         
         
