cbe232049589c8f69b6cd695cb5ab20cecefc646
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-18 11:06:36 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -6,41 +6,24 @@ using namespace std;
 class Solution
 {
     public:
-    typedef struct meeting{
-        int start;
-        int end;
-        int pos;
-    };
-    static bool comparator( meeting m1,meeting m2){
-        if(m1.end<m2.end)
-            return true;
-        if(m1.end>m2.end)
-            return false;
-        if(m1.pos>m2.pos)
-            return false;
-        return true;
-    }
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
     int maxMeetings(int start[], int end[], int n)
     {
-        meeting m[n];
+        vector<pair<int,int>> vp(n);
         for(int i=0;i<n;i++){
-            m[i].start=start[i];
-            m[i].end=end[i];
-            m[i].pos=i+1;
+            vp[i]={start[i],end[i]};
         }
-        sort(m,m+n,comparator);
+        sort(vp.begin(),vp.end(),[](const pair<int,int> &a,const pair<int,int>&b){
+            return a.second<b.second;
+        });
         int ans=1;
-        int current_end=m[0].end;
-        // for(int i=0;i<n;i++){
-        //     cout<<m[i].start<<" "<<m[i].end<<"\n";
-        // }
+        int curr=0;
         for(int i=1;i<n;i++){
-            if(m[i].start>current_end){
-                current_end=m[i].end;
-                ans++;
-            }
+          if(vp[i].first>vp[curr].second){
+              ans++;
+              curr=i;
+          }  
         }
         return ans;
         // Your code here
