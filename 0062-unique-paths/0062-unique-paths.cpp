8637d93dbf4d2ad8e36d8cd1a78e46d319d47212
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-27 02:44:10 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (19.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,7 +12,23 @@ public:
         return dp[i][j]=right+down;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        // vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[0][0]=1;
+                else{
+                    int left=0;
+                    int up=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
         return solve(m,n,0,0,dp);
     }
 };
