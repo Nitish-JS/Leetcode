37c6a60f332b7e4a0b776234876146eb491ff96c
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-27 02:32:21 +0530

    Time: 5 ms (23.02%), Space: 6.4 MB (63.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-    // int solve(int i,int j,vector<vector<int>>&dp){
-    //     if(i==0 && j==0)
-    //         return 1;
-    //     if(i<0|| j<0)
-    //         return 0;
-    //     if(dp[i][j]!=-1)
-    //         return dp[i][j];
-    //     int right=solve(i,j-1,dp);
-    //     int down=solve(i-1,j,dp);
-    //     return dp[i][j]=right+down;
-    // }
+    int solve(int m,int n,int i,int j,vector<vector<int>> &dp){
+        if(i==m-1 && j==n-1)
+            return 1;
+        if(i>=m || j>=n)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int right=solve(m,n,i,j+1,dp);
+        int down=solve(m,n,i+1,j,dp);
+        return dp[i][j]=right+down;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        vector<int> curr(n,0);
-        vector<int> prev(n,0);
-        // dp[0][0]=1;
-        curr[0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i!=0 || j!=0){
-                    int right=0;
-                    if(j>0)
-                        right=curr[j-1];
-                    int down=0;
-                    if(i>0)
-                        down=prev[j];
-                    curr[j]=right+down;
-                }
-               prev=curr;
-            }
-        }
-        return curr[n-1];
-        // return solve(m-1,n-1,dp);
-        
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m,n,0,0,dp);
     }
 };
