3e280a5dbecffd717eb66e9aca59f61c50f27dc5
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-27 02:51:58 +0530

    Time: 8 ms (94.50%), Space: 10.8 MB (44.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> um;
         for(int i=0;i<nums.size();i++){
-            if(um.find(target-nums[i])!=um.end())
+            if(um.find(target-nums[i])!=um.end()){
                 return {i,um[target-nums[i]]};
+            }
             else
                 um[nums[i]]=i;
         }
-        return {0,0};
+        return {-1};
     }
 };
