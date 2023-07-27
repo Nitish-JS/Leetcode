d4545c06c805371ce60fd4bdd14593692b2a9048
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-27 16:31:15 +0530

    Time: 207 ms (56.97%), Space: 24.1 MB (71.11%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,7 +6,7 @@ public:
         int n=nums.size();
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            if(i==0 || nums[i]!=nums[i-1]){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 int low=i+1;
                 int high=n-1;
                 while(low<high){
