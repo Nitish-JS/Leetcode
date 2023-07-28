22e579cda6f43e2230c68ae1c661da772e96344d
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-28 16:23:13 +0530

    Time: 15 ms (98.54%), Space: 14.2 MB (33.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    bool isCycle(vector<vector<int>> &graph,vector<int> &visited,vector<int> &dfsVisited,int node){
+    void buildGraph(vector<int> adj[],vector<vector<int>> &prereq){
+        for(auto edge:prereq){
+            adj[edge[1]].push_back(edge[0]);
+        }
+    }
+    bool detectCycleDfs(vector<int> adj[],int node,vector<int> &visited,vector<int> &dfsVisited){
         visited[node]=1;
         dfsVisited[node]=1;
-        for(auto it:graph[node]){
+        for(auto it:adj[node]){
             if(!visited[it]){
-                if(isCycle(graph,visited,dfsVisited,it))
+                if(detectCycleDfs(adj,it,visited,dfsVisited)==true)
                     return true;
             }
             else if(dfsVisited[it])
@@ -15,18 +20,16 @@ public:
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V=numCourses;
-        vector<vector<int>> graph(V);
-        for(auto edge:prerequisites){
-            graph[edge[1]].push_back(edge[0]);
-        }        
-        vector<int> visited(V);
-        vector<int> dfsVisited(V);
-        for(int i=0;i<V;i++){
-            if(!visited[i] && isCycle(graph,visited,dfsVisited,i))
-                return false;
+        vector<int> adj[numCourses];
+        buildGraph(adj,prerequisites);
+        vector<int> visited(numCourses);
+        vector<int> dfsVisited(numCourses);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(detectCycleDfs(adj,i,visited,dfsVisited)==true)
+                    return false;
+            }
         }
         return true;
-        
     }
 };
