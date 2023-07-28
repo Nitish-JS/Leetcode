81b12a9a290770ed17a8da4fc345018374753095
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-07-28 10:47:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int left,int right,int chance,vector<vector<vector<int>>> &dp){
+    int solve(vector<int> &nums,int left,int right,int chance,vector<vector<int>> &dp){
         if(left>right)
             return 0;
-        if(dp[left][right][chance]!=-1)
-            return dp[left][right][chance];
+        if(dp[left][right]!=-1)
+            return dp[left][right];
         if(chance==0)
-            return dp[left][right][chance]=min(solve(nums,left+1,right,1,dp),solve(nums,left,right-1,1,dp));
+            return dp[left][right]=min(solve(nums,left+1,right,1,dp),solve(nums,left,right-1,1,dp));
         else
-            return dp[left][right][chance]=max(nums[left]+solve(nums,left+1,right,0,dp),nums[right]+solve(nums,left,right-1,0,dp));
+            return dp[left][right]=max(nums[left]+solve(nums,left+1,right,0,dp),nums[right]+solve(nums,left,right-1,0,dp));
     }
     bool PredictTheWinner(vector<int>& nums) {
         int alice=0;
         int bob=0;
         for(int i:nums)
             bob+=i;
-        vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>>(nums.size(),vector<int>(2,-1)));
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
+        // vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>>(nums.size(),vector<int>(2,-1)));
         alice=solve(nums,0,nums.size()-1,1,dp);
         bob-=alice;
         cout<<alice<<" "<<bob<<'\n';
