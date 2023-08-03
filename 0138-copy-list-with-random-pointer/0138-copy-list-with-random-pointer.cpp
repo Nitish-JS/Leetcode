2b04dd4eeadee37bfbad991fe56d33917d55e05f
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-03 16:30:56 +0530

    Time: 10 ms (51.84%), Space: 11.1 MB (86.96%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,19 +17,50 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> um;
+        // space complexity O(n)
+        // unordered_map<Node*,Node*> um;
+        // Node* temp=head;
+        // while(temp){
+        //     um[temp]=new Node(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp){
+        //     um[temp]->next=um[temp->next];
+        //     um[temp]->random=um[temp->random];
+        //     temp=temp->next;
+        // }
+        // return um[head];
+        
+        // space complexity O(1)
+        
         Node* temp=head;
         while(temp){
-            um[temp]=new Node(temp->val);
-            temp=temp->next;
+            Node* newNode=new Node(temp->val);
+            newNode->next=temp->next;
+            temp->next=newNode;
+            temp=temp->next->next;
         }
         temp=head;
-        while(temp){
-            um[temp]->next=um[temp->next];
-            um[temp]->random=um[temp->random];
-            temp=temp->next;
+        while(temp && temp->next){
+            if(temp->random==NULL)
+                temp->next->random=NULL;
+            else
+                temp->next->random=temp->random->next;
+            temp=temp->next->next;
         }
-        return um[head];
-    
+        Node* deepCopy=new Node(0);
+        Node* copy=deepCopy;
+        temp=head;
+        while(temp){
+            Node* front=temp->next->next;
+            copy->next=temp->next;
+            temp->next=front;
+            copy=copy->next;
+            temp=temp->next;
+            
+        }
+        return deepCopy->next;
+        
     }
 };
