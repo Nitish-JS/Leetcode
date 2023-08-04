ea6a8c3905e27601df36276774f8b022eb42927c
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-04 23:17:31 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (35.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // count no of ones
-        int oneCount=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        //copying the input matrix
-        vector<vector<int>> mat(n,vector<int>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mat[i][j]=grid[i][j];
-            }
-        }
+        int m=grid.size();
+        int n=grid[0].size();
+        int one_count=0;
+        vector<vector<int>> mat(m,vector<int>(n));
         queue<pair<int,int>> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==2){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]=grid[i][j];
+                if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                else if(mat[i][j]==1)
-                    oneCount++;
+                else if(grid[i][j]==1){
+                    one_count++;
+                }
             }
         }
-        if(!oneCount)
+        if(one_count==0)
             return 0;
+        int disX[]={-1,0,1,0};
+        int disY[]={0,-1,0,1};
+        int new_one_count=0;
         int time=0;
-        int curOneCount=0;
-        vector<int> rows={-1,0,1,0};
-        vector<int> cols={0,-1,0,1};
         while(!q.empty()){
             int size=q.size();
             for(int i=0;i<size;i++){
-                pair<int,int> node=q.front();
+                pair<int,int> currNode=q.front();
                 q.pop();
-                int currRow=node.first;
-                int currCol=node.second;
+                int currRow=currNode.first;
+                int currCol=currNode.second;
                 for(int i=0;i<4;i++){
-                    int nextRow=currRow+rows[i];
-                    int nextCol=currCol+cols[i];
-                    if(nextRow>=0 && nextCol>=0 && nextRow<n && nextCol<m && mat[nextRow][nextCol]==1){
-                        q.push({nextRow,nextCol});
-                        mat[nextRow][nextCol]=2;
-                        curOneCount++;
+                    int newRow=currRow+disX[i];
+                    int newCol=currCol+disY[i];
+                    if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && mat[newRow][newCol]==1){
+                        q.push({newRow,newCol});
+                        mat[newRow][newCol]=2;
+                        new_one_count++;
                     }
                 }
             }
             time++;
         }
-        if(oneCount!=curOneCount)
+        if(new_one_count!=one_count)
             return -1;
         return time-1;
     }
