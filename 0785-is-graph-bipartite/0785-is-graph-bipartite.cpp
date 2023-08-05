09fa85a96cf3f46c34183babcb576168f859c5ce
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-05 19:06:10 +0530

    Time: 11 ms (99.82%), Space: 13.6 MB (48.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
+    // dfs approach
+    // bool check(vector<vector<int>> &graph,vector<int> &colors,int node,int color){
+    //     colors[node]=color;
+    //     for(auto it:graph[node]){
+    //         if(colors[it]==-1){
+    //             if(check(graph,colors,it,!color)==false)
+    //                 return false;
+    //         }
+    //         else if(colors[it]==colors[node])
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    
+    
+    // bfs approach
+    
     bool check(vector<vector<int>> &graph,vector<int> &colors,int node,int color){
         colors[node]=color;
-        for(auto it:graph[node]){
-            if(colors[it]==-1){
-                if(check(graph,colors,it,!color)==false)
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:graph[node]){
+                if(colors[it]==-1){
+                    colors[it]=!colors[node];
+                    q.push(it);
+                }
+                else if(colors[it]==colors[node])
                     return false;
             }
-            else if(colors[it]==colors[node])
-                return false;
         }
         return true;
     }
