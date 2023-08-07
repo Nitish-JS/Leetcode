a2dbddf4f2f89e3fe517e263884b8256e10df7a0
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-07 09:00:12 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& arr, int target) {
-        int m=arr.size();
-        int n=arr[0].size();
-        int low=0,high=m*n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            int midValue=arr[mid/n][mid%n];
-            if(midValue==target)
-                return true;
-            else if(midValue<target)
-                low=mid+1;
-            else
-                high=mid-1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size()-1;
+        int n=matrix[0].size()-1;
+        int row_low=0,row_high=m;
+        while(row_low<=row_high){
+            int col_low=0,col_high=n;
+            int row_mid=row_low+(row_high-row_low)/2;
+            if(matrix[row_mid][0]>target)
+                row_high=row_mid-1;
+            else if(matrix[row_mid][0]<=target)
+                row_low=row_mid+1;
+            while(col_low<=col_high){
+                int col_mid=col_low+(col_high-col_low)/2;
+                if(matrix[row_mid][col_mid]==target)
+                    return true;
+                else if(matrix[row_mid][col_mid]>target)
+                    col_high=col_mid-1;
+                else
+                    col_low=col_mid+1;
+            }
         }
         return false;
-        
     }
 };
