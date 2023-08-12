98ab04d4d79a59d3cba242eefb7cb6ec7dd34c78
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-12 19:52:05 +0530

    Time: 10 ms (60.48%), Space: 10 MB (44.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &grid,int row,int col){
-        if(row==0 && col==0){
-            return grid[0][0];
-        }
-        if(row<0 || col<0)
-            return 1e5;
-        int up=solve(grid,row-1,col);
-        int left=solve(grid,row,col-1);
-        return grid[row][col]+min(up,left);
-    }
+    // int solve(vector<vector<int>> &grid,int row,int col){
+    //     if(row==0 && col==0){
+    //         return grid[0][0];
+    //     }
+    //     if(row<0 || col<0)
+    //         return 1e5;
+    //     int up=solve(grid,row-1,col);
+    //     int left=solve(grid,row,col-1);
+    //     return grid[row][col]+min(up,left);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0]=grid[0][0];
+        // vector<vector<int>> dp(m,vector<int>(n,0));
+        // dp[0][0]=grid[0][0];
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0)
-                    continue;
-                int right=1e5;
-                int down=1e5;
-                if(j>0)
-                    right=dp[i][j-1];
-                if(i>0)
-                    down=dp[i-1][j];
-                dp[i][j]=grid[i][j]+min(right,down);
+                    curr[0]=grid[i][j];
+                else{
+                    int right=1e5;
+                    int down=1e5;
+                    if(j>0)
+                        right=curr[j-1];
+                    if(i>0)
+                        down=prev[j];
+                    curr[j]=grid[i][j]+min(right,down);
+                }
+               
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
-        return solve(grid,m-1,n-1);
+        return prev[n-1];
+        // return dp[m-1][n-1];
+        // return solve(grid,m-1,n-1);
     }
 };
