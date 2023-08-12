91d5618bbac8da699a3f88c28cf743d47b89d616
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-12 10:47:30 +0530

    Time: 6 ms (19.48%), Space: 7.6 MB (81.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &grid,int row,int col,vector<vector<int>> &dp){
-        if(row==0 && col==0)
-            return 1;
-        if(row<0 || col<0 || grid[row][col]==1)
-            return 0;
-        if(dp[row][col]!=-1)
-            return dp[row][col];
-        int up=solve(grid,row-1,col,dp);
-        int left=solve(grid,row,col-1,dp);
-        return dp[row][col]=up+left;
-    }
+    // int solve(vector<vector<int>> &grid,int row,int col,vector<vector<int>> &dp){
+    //     if(row==0 && col==0)
+    //         return 1;
+    //     if(row<0 || col<0 || grid[row][col]==1)
+    //         return 0;
+    //     if(dp[row][col]!=-1)
+    //         return dp[row][col];
+    //     int up=solve(grid,row-1,col,dp);
+    //     int left=solve(grid,row,col-1,dp);
+    //     return dp[row][col]=up+left;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if(obstacleGrid[0][0]==1)
             return 0;
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0]=1;
+        // vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> curr(n,0);
+        vector<int> prev(n,0);
+        curr[0]=1;
         for(int row=0;row<m;row++){
             for(int col=0;col<n;col++){
-                if((col==0 && row==0) || obstacleGrid[row][col]==1)
-                    continue;
+                if(col==0 && row==0)
+                    curr[0]=1;
+                else if(obstacleGrid[row][col]==1)
+                    curr[col]=0;
                 else {
                     int right=0;
                     int down=0;
                     if(row>0 )
-                        down=dp[row-1][col];
+                        down=prev[col];
                     if(col>0)
-                        right=dp[row][col-1];
-                    dp[row][col]=right+down;
+                        right=curr[col-1];
+                    curr[col]=right+down;
                 }
-                
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
-        return solve(obstacleGrid,m-1,n-1,dp);
+        return curr[n-1];
+        // return solve(obstacleGrid,m-1,n-1,dp);
     }
 };
