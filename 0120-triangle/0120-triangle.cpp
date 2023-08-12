1fd862f25e20a1a98d0d4248f16f3794456d5781
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-12 20:13:08 +0530

    Time: 8 ms (51.75%), Space: 9 MB (19.74%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    // int solve(vector<vector<int>> &triangle,int i,int j,int n,vector<vector<int>> &dp){
-    //     if(n==0)
-    //         return triangle[i][j];
-    //     if(dp[i][j]!=-1)
-    //         return dp[i][j];
-    //     int down=triangle[i][j]+solve(triangle,i+1,j,n-1,dp);
-    //     int diag=triangle[i][j]+solve(triangle,i+1,j+1,n-1,dp);
-    //     return dp[i][j]=min(down,diag);
-    // }
+    int solve(vector<vector<int>> &grid,int row,int col,vector<vector<int>> &dp){
+        if(row==grid.size()-1)
+            return grid[row][col];
+        if(row>=grid.size() || col>grid[row].size())
+            return 1e5;
+        if(dp[row][col]!=-1)
+            return dp[row][col];
+        int down=solve(grid,row+1,col,dp);
+        int leftdown=solve(grid,row+1,col+1,dp);
+        return dp[row][col]=grid[row][col]+min(down,leftdown);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int> dp(n,0);
-        dp[0]=triangle[0][0];
-        for(int i=1;i<triangle.size();i++){
-            vector<int> temp(n);
-            for(int j=0;j<i+1;j++){
-                int up=INT_MAX;
-                if(j!=i){
-                    up=triangle[i][j]+dp[j];
-                }
-                int diag=INT_MAX;
-                if(j>0)
-                    diag=triangle[i][j]+dp[j-1];
-                temp[j]=min(up,diag);
-            }
-            dp=temp;
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            ans=min(dp[i],ans);
-        }
-        return ans;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(triangle,0,0,dp);
     }
-}; 
+};
