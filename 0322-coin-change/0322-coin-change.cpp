088056e100e03a25ae7f29cc1b626c6d338e1a69
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-13 16:39:16 +0530

    Time: 111 ms (53.15%), Space: 45.1 MB (34.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -14,7 +14,22 @@ public:
         return dp[index][amount]=min(take,nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        // vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,1e9));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+                dp[0][i]=i/coins[0];
+        }
+        for(int index=1;index<coins.size();index++){
+            for(int money=0;money<=amount;money++){
+                int take=INT_MAX;
+                if(coins[index]<=money)
+                    take=1+dp[index][money-coins[index]];
+                int nottake=dp[index-1][money];
+                dp[index][money]=min(take,nottake);
+            }
+        }
+        return dp[coins.size()-1][amount]==1e9?-1:dp[coins.size()-1][amount];
         int ans=solve(coins,coins.size()-1,amount,dp);
         return ans==1e9?-1:ans;
     }
