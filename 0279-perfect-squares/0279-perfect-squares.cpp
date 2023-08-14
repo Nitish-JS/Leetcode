6f0099e8be4b06cdf068b0acff63d7800809103c
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-14 18:30:25 +0530

    Time: 282 ms (34.84%), Space: 9.8 MB (31.14%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int> &squares,int index,int target,vector<vector<int>> &dp){
-     
-        if(index==0){
-            if(target==0)
-                return 0;
-            if(target%squares[index]==0)
-                return target/squares[index];
-            return 1e5;
+    int solve(vector<int> &dp,int index,int target){
+        if(target==0)
+            return 0;
+        if(dp[target]!=-1)
+            return dp[target];
+        if(index*index<=target){
+            return dp[target]=min(1+solve(dp,index,target-index*index),solve(dp,index+1,target));
         }
-        if(dp[index][target]!=-1)
-            return dp[index][target];
-        int take=INT_MAX;
-        if(squares[index]<=target){
-            take=1+solve(squares,index,target-squares[index],dp);
-        }
-        int nottake=0+solve(squares,index-1,target,dp);
-        return dp[index][target]=min(take,nottake);
-        
-        
+        return dp[target]=1e5;
     }
     int numSquares(int n) {
-       vector<int> squares;
-        for(int i=1;i<=n;i++){
-            if(sqrt(i)==floor(sqrt(i)))
-                squares.push_back(i);
-        }
-        vector<vector<int>> dp(squares.size(),vector<int>(n+1,-1));
-        return solve(squares,squares.size()-1,n,dp);
-        
+        vector<int>dp(n+1, -1);
+        return solve(dp,1,n);
     }
 };
