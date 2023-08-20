071bd699078d02f3828ce7f24375b3cf7fddb586
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-08-20 22:37:43 +0530

    Time: 11 ms (42.27%), Space: 9.4 MB (66.19%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones=0;
-        int twos=0;
-        for(int i=0;i<nums.size();i++){
-            ones=ones^nums[i]&(~twos);
-            twos=twos^nums[i]&(~ones);
+        // int ones=0;
+        // int twos=0;
+        // for(int i=0;i<nums.size();i++){
+        //     ones=ones^nums[i]&(~twos);
+        //     twos=twos^nums[i]&(~ones);
+        // }
+        // return ones;
+        int num=0;
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if((1<<i)&nums[j])
+                    count++;
+            }
+            if(count%3){
+                num=num|(1<<i);
+            }
         }
-        return ones;
+        return num;
     }
 };
