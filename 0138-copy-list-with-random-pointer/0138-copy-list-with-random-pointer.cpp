19de4090dcba4026096e772a61129152524e6263
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-09-05 11:35:14 +0530

    Time: 3 ms (96.91%), Space: 11.2 MB (60.38%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -18,49 +18,21 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         // space complexity O(n)
-        // unordered_map<Node*,Node*> um;
-        // Node* temp=head;
-        // while(temp){
-        //     um[temp]=new Node(temp->val);
-        //     temp=temp->next;
-        // }
-        // temp=head;
-        // while(temp){
-        //     um[temp]->next=um[temp->next];
-        //     um[temp]->random=um[temp->random];
-        //     temp=temp->next;
-        // }
-        // return um[head];
-        
-        // space complexity O(1)
-        
+        unordered_map<Node*,Node*> um;
         Node* temp=head;
         while(temp){
-            Node* newNode=new Node(temp->val);
-            newNode->next=temp->next;
-            temp->next=newNode;
-            temp=temp->next->next;
+            um[temp]=new Node(temp->val);
+            temp=temp->next;
         }
-        temp=head;
-        while(temp && temp->next){
-            if(temp->random==NULL)
-                temp->next->random=NULL;
-            else
-                temp->next->random=temp->random->next;
-            temp=temp->next->next;
-        }
-        Node* deepCopy=new Node(0);
-        Node* copy=deepCopy;
         temp=head;
         while(temp){
-            Node* front=temp->next->next;
-            copy->next=temp->next;
-            temp->next=front;
-            copy=copy->next;
+            um[temp]->next=um[temp->next];
+            um[temp]->random=um[temp->random];
             temp=temp->next;
-            
         }
-        return deepCopy->next;
+        return um[head];
+        
+       
         
     }
 };
