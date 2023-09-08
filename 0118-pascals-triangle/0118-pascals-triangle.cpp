a94c3d473207534783deca5bcc66b5c1a2a97d58
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-09-08 11:36:25 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (82.50%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,13 +3,11 @@ public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            vector<int> temp(i+1,1);
-            if(i>1){
-                for(int j=1;j<i;j++){
-                    temp[j]=ans[i-1][j-1]+ans[i-1][j];
-                }
+            vector<int> row(i+1,1);
+            for(int j=1;j<i;j++){
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
-            ans.push_back(temp);
+            ans.push_back(row);
         }
         return ans;
     }
