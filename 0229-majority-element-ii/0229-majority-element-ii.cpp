da85bce6be8df64139e5f3771ff66a6121b95954
Nitish <73771450+Nitish-JS@users.noreply.github.com>
2023-10-06 21:46:28 +0530

    Time: 6 ms (89.98%), Space: 16.2 MB (19.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int candidate1=-1,vote1=0;
-        int candidate2=-1,vote2=0;
-        for(int i:nums){
-            if(vote1==0 && i!=candidate2){
-                candidate1=i;
-                vote1=1;
-            }
-            else if(vote2==0 && i!=candidate1){
-                candidate2=i;
-                vote2=1;
-            }
-            else if(candidate1==i)
+        int candidate1=-1,candidate2=-1;
+        int vote1=0,vote2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(vote1==0 && nums[i]!=candidate2){
+                candidate1=nums[i];
                 vote1++;
-            else if(candidate2==i)
+            }
+            else if(vote2==0 && nums[i]!=candidate1){
+                candidate2=nums[i];
+                vote2++;
+            }
+            else if(nums[i]==candidate1)
+                vote1++;
+            else if(nums[i]==candidate2)
                 vote2++;
             else{
                 vote1--;
                 vote2--;
             }
-             // cout<<candidate1<<" "<<candidate2<<'\n';
         }
         int count1=0,count2=0;
-        for(int i:nums){
-            if(i==candidate1)
+        for(int i=0;i<n;i++){
+            if(nums[i]==candidate1)
                 count1++;
-            else if(i==candidate2)
+            else if(nums[i]==candidate2)
                 count2++;
-            
         }
-       
         vector<int> ans;
-        if(count1>nums.size()/3)
+        if(count1>n/3)
             ans.push_back(candidate1);
-        if(count2>nums.size()/3)
+        if(count2>n/3)
             ans.push_back(candidate2);
         return ans;
     }
